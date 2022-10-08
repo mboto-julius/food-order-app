a88fcf8c741fea8c2cc22a51fd20be7d3f7ed29c
Julius Mboto <juliusmboto@gmail.com>
2022-10-08 22:55:14 +0300

    add template admin panel

---
+++ admin/index.php
@@ -0,0 +1,37 @@
+<?php
+
+include('includes/menu.php');
+
+?>
+
+<!-- Main Section Starts -->
+<div class="main">
+    <div class="wrapper">
+        <h1>Dashboard</h1>
+        <div class="main-col">
+            <div class="col-4 text-center">
+                <h1>5</h1><br>
+                <p>Categories</p>
+            </div>
+            <div class="col-4 text-center">
+                <h1>5</h1><br>
+                <p>Categories</p>
+            </div>
+            <div class="col-4 text-center">
+                <h1>5</h1><br>
+                <p>Categories</p>
+            </div>
+            <div class="col-4 text-center">
+                <h1>5</h1><br>
+                <p>Categories</p>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Main Section Ends -->
+
+<?php
+
+include('includes/footer.php');
+
+?>
---
+++ admin/manage-food.php
@@ -0,0 +1,19 @@
+<?php
+
+include('includes/menu.php');
+
+?>
+
+<!-- Main Section Starts -->
+<div class="main">
+    <div class="wrapper">
+        <h1>Manage Food</h1>
+    </div>
+</div>
+<!-- Main Section Ends -->
+
+<?php
+
+include('includes/footer.php');
+
+?>
